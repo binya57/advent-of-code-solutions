563ee63af01aaadaba787b44f25a02c295e723e9
binya <binya57+dev@gmail.com>
2024-02-26 00:01:52 +0200

    expand solution

--- 2023/3/#part1.js
+++ 2023/3/#part1.js
@@ -1,39 +1,90 @@
 //@ts-check
+/**
+ * @typedef TNumberMatch
+ * @property {number} value
+ * @property {number} startIndex
+ * @property {number} length
+ */
 
 
 /**
  * 
  * @param {string[]} matrix 
- * @return {[number, number, number][][]}
+ * @return {TNumberMatch[][]}
  */
 function getIndices(matrix) {
     return matrix.reduce(
-        (/**@type {[number, number, number][][]} */indices, row) => {
+        (/**@type {TNumberMatch[][]} */indices, row) => {
             indices.push([]);
-            const numbersMatches = [...row.matchAll(/\d+/g)];
+            const numbersMatches = [...row.join('').matchAll(/\d+/g)];
             numbersMatches.map((match) => {
                 const [number] = match;
                 const index = match.index || 0;
                 const matchLength = number.length;
-                indices[indices.length - 1].push([+number, index, matchLength]);
+                indices[indices.length - 1].push({ value: +number, startIndex: index, length: matchLength });
             })
             return indices;
         }, [])
 }
 
-/**
- * @typedef {{value: number, }} TNumberMatch
- */
 
 /**
  * 
- * @param {*} matrix 
- * @param {*} index 
+ * @param {string[]} matrix 
+ * @param {number} row 
+ * @param {number} index 
  */
-function getAdjacentIndices(matrix, index) {
+function getAdjacentIndices(matrix, row, index) {
+    return (
+        [
+            matrix[row][index + 1], // next in row
+            matrix[row - 1][index + 1], // topRight
+            matrix[row + 1][index + 1], // bottomRight
+            matrix[row][index - 1], // prev in row
+            matrix[row - 1][index - 1], // topLeft
+            matrix[row + 1][index - 1], // bottomLeft
+            matrix[row - 1][index], // top
+            matrix[row + 1][index], // bottom
+        ].filter(Boolean)
+    )
+}
+
+/**
+ * 
+ * @param {string[]} matrix 
+ */
+function getPartNumbers(matrix) {
+    let partNumbers = [];
+    const numbersIndices = getIndices(matrix);
+    numbersIndices.forEach((row, index) => {
+        row.forEach(numberMatch => {
+            const { value, startIndex, length } = numberMatch;
+            let isPartNumber = false;
+            for (let i = startIndex; i < startIndex + length - 1; i++) {
+                const adjacent = getAdjacentIndices(matrix, index, i);
+                const hasAdjacentSymbol = adjacent.some(value => /[^\d\.]+/g.test(value))
+                if (adjacent.some(char => !isNaN(+char))) { // testing only
+                    throw new Error("has adjacent number");
+                }
+                if (hasAdjacentSymbol) {
+                    isPartNumber = true;
+                    break;
+                }
+
+            }
+            if (isPartNumber) {
+                partNumbers.push(value);
+            }
+
+        })
+    })
+
+    return partNumbers;
 
 }
 
 module.exports = {
     getIndices,
+    getAdjacentIndices,
+    getPartNumbers
 }
--- 2023/3/#part1.test.js
+++ 2023/3/#part1.test.js
@@ -1,19 +1,73 @@
-const { getIndices } = require("./#part1")
+const { getIndices, getAdjacentIndices, getPartNumbers } = require("./#part1")
 
 test('get matrix indices', () => {
     expect(getIndices(
         [
-            '1213asdasd123333asda'
+            '123a123'
         ])).toEqual([
             [
-                1213,
-                0,
-                4
-            ],
-            [
-                123333,
-                10,
-                6
+                { value: 123, startIndex: 0, length: 3 },
+                { value: 123, startIndex: 4, length: 3 }
             ]
         ])
+})
+
+test('get Adjacent Indices', () => {
+    const test = [
+        ['*', '*', '*',],
+        ['*', '5', '*',],
+        ['*', '*', '*',],
+    ]
+    expect(getAdjacentIndices(test, 1, 1,)).toEqual(new Array(8).fill("*"));
+})
+
+test('get part numbers ', () => {
+    const test1 = [
+        ['.', '.', '.',],
+        ['.', '5', '.',],
+        ['.', '.', '.',],
+    ]
+    const test2 = [
+        ['*', '*', '*',],
+        ['*', '5', '*',],
+        ['*', '*', '*',],
+    ]
+
+    const test3 = [
+        ['*', '.', '.',],
+        ['.', '5', '.',],
+        ['.', '.', '.',],
+    ]
+
+    const test4 = [
+        ['*', '.', '.',],
+        ['.', '5', '.',],
+        ['.', '.', '.',],
+    ]
+
+    const test5 = [
+        ['*', '.', '.',],
+        ['.', '5', '.',],
+        ['.', '.', '7',],
+    ]
+
+    const test6 = [
+        ['*', '.', '.',],
+        ['.', '.', '.',],
+        ['.', '.', '7',],
+    ]
+
+    const tests = [
+        test1, test2, test3, test4
+    ]
+
+    // for (let test of tests) {
+    expect(getPartNumbers(test1)).toEqual([]);
+    expect(getPartNumbers(test2)).toEqual([5]);
+    expect(getPartNumbers(test3)).toEqual([5]);
+    expect(getPartNumbers(test4)).toEqual([5]);
+    expect(getPartNumbers(test5)).toThrow("has adjacent number")
+    expect(getPartNumbers(test6)).toEqual([])
+
+    // }
 })
